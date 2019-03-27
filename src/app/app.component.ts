d1f037fd02e158e1ee08dfac28bfb5273559985f
bhupendrapatel <bhupendra.patel@neosofttech.com>
2019-03-27 12:45:47 +0530

    formio added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'formDemo';
+  title = "formDemo";
+  @ViewChild("json") jsonElement?: ElementRef;
+  public form: Object = { components: [] };
+  onChange(event) {
+    console.log(event.form);
+    if (event.changed != "undefined") {
+      this.jsonElement.nativeElement.innerHTML = "";
+      this.jsonElement.nativeElement.appendChild(
+        document.createTextNode(JSON.stringify(event.form, null, 4))
+      );
+    }
+  }
 }
